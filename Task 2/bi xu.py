cbb6bf01c2df374be47ff6f933b2e9a2a836c313
weichengwillie <weicheng911011@gmail.com>
2024-04-26 14:43:34 +0800

    初步的，但還有些問題0.0

--- Task 2/bi xu.py
+++ Task 2/bi xu.py
@@ -1,7 +1,43 @@
+import re
+
+#讀取檔案，並回傳 raw list
 def inputList():
     with open('bi_xu_raw.txt', 'r') as fh:
-        raw = fh.read().split('more')
-    lines = []
-    for i in raw:
-        i = i.replace('\n', '').replace('\t', '')
-        lines.append(i)
+        #刪去整個txt檔中的\n\t，並以'more'區分
+        raw = fh.read().replace('\n', '').replace('\t', '').split('more')
+    return raw
+    
+#輸入並處理raw list、回傳processed list
+def process(raw):
+    processed = []
+    for line in raw:
+        pattern = '，|。|！|？|；'
+        #把每一行依照標點符號(pattern)分成段（類似子句）
+        parts = re.split(pattern, line,)
+        for part in parts:
+            #選取含有"必須"的片段，並append到processed lsit
+            if "必須" in part:
+                processed.append(part)
+            if "「
+    #轉成集合已刪除重複句，再轉回list
+    processed = list(set(processed))
+    return processed
+    
+#呈現processed list
+def show(processed):
+    for s in processed:
+        print(f'{s}')
+
+#將processed list輸出txt檔
+def save(processed):
+    with open('bi_xu_processed.txt', 'w') as fh:
+        sentence = [f'{s}\n' for s in processed]
+        fh.writelines(sentence)
+        
+
+                
+
+raw_list = inputList()
+processed_list = process(raw_list)
+show(processed_list)
+save(processed_list)
